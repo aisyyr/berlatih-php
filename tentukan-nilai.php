2fa46947ea22e218e4896a3311d19c6906413d90
aisyyr <aisyyr0620@gmail.com>
2021-02-25 08:45:07 +0700

    tes membuat file latihan sanbercode

---
+++ tentukan-nilai.php
@@ -0,0 +1,22 @@
+<?php
+function tentukan_nilai($number)
+{
+    //  kode disini
+    if($number <= 50){
+        echo "Kurang <br>";
+    } elseif ($number <= 70) {
+        echo "Cukup <br>";
+    } elseif ($number <= 85) {
+        echo "Baik <br>";
+    } else {
+        echo "Sangat Baik <br>";
+    }
+    
+}
+
+//TEST CASES
+echo tentukan_nilai(98); //Sangat Baik
+echo tentukan_nilai(76); //Baik
+echo tentukan_nilai(67); //Cukup
+echo tentukan_nilai(43); //Kurang
+?>
---
+++ tukar-besar-kecil.php
@@ -0,0 +1,19 @@
+<?php
+function tukar_besar_kecil($string){
+    //kode di sini
+    $abjad = "abcdefghijklmnopqrstuvwxyz";
+    $output ="";
+    for ($i=0; $i < strlen($string); $i++) { 
+        $position = strpos($abjad, $string
+        [$i]);
+        $output = substr($abjad, $position+1, 1);
+    }
+    return $output."<br>";
+}
+
+// TEST CASES
+echo tukar_besar_kecil('Hello World'); // "hELLO wORLD"
+echo tukar_besar_kecil('I aM aLAY'); // "i Am Alay"
+echo tukar_besar_kecil('My Name is Bond!!'); // "mY nAME IS bOND!!"
+echo tukar_besar_kecil('IT sHOULD bE me'); // "it Should Be ME"
+echo tukar_besar_kecil('001-A-3-5TrdYW'); // "001-a-3-5tRDyw"
